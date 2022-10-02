127b592197d18adf06616a4341c0752a810850b7
theheroman666 <93671904+theheroman666@users.noreply.github.com>
2022-10-02 14:50:56 -0500

    Delete AMG-page directory

--- AMG-page/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AMG_page.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
